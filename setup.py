a552950ea8873b00faa0b8be7ee816bc80225a8a
Karsten Hoffrath <khoffrath@khoffrath.de>
2016-10-14 21:54:33 +0200

    Version bump.
    Update documentation, changelog and setup.py to reflect the latest
    changes.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 
 setuptools.setup(
     name='jsondate',
-    version='0.1.2',
-    url='https://github.com/rconradharris/jsondate',
+    version='0.1.3',
+    url='https://github.com/khoffrath/jsondate',
     license='MIT',
     author='Rick Harris',
     author_email='rconradharris@gmail.com',
-    description='JSON with datetime support',
+    description='JSON with datetime support (Python 3 compatible)',
     long_description=__doc__,
     packages=setuptools.find_packages(),
     zip_safe=False,
